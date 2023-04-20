749ca108ca56d1d5b79750623380b011125b196f
dhairyassheth <19bee116@nirmauni.ac.in>
2023-04-20 15:07:08 +0530

    Day-1

---
+++ Day-1/main.py
@@ -0,0 +1,11 @@
+print("""
+Dhairya
+April 20th 2023
+
+I am signing up for Repli's 100 days of Python Challenge!
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happened to be.
+No excuses for not coding from the middle of field
+I am feeling 🫡
+You can follow my progress at replit.com/@SHETHDHAIRYA or github.com/dhairyassheth
+""")
